6fe4b120b02b12c7376bcdb6ea75c4dcacc1498c
Packet Echo <packetecho@gmail.com>
2018-12-20 09:22:23 +0000

    Hardcode site vars to reduce queries

--- header.php
+++ header.php
@@ -1,32 +1,32 @@
-<?php
-/**
- * The header for our theme.
- *
- */
-
-?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html lang="en-GB">
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset=""UTF-8"">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
-	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
+	<meta name="apple-mobile-web-app-title" content="Duns Tew - <?php bloginfo( 'description' ); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="pingback" href="<?php echo site_url(); ?>/xmlrpc.php">
 	<link rel="author" href="<?php echo site_url(); ?>/humans.txt">
+	<meta name="theme-color" content="#5e72e4">
+
+	<meta name="google-site-verification" content="0w3-bNTp0Jc3I1fI_FDTotygnDQ6gz2Lu9kDSJCpgSM">
+	<meta name="msvalidate.01" content="685FAC32773137C94DC728D2774C36A6">
+	<meta name='wot-verification' content='d13818041bdbdf382c5e'>
+	<meta name='twitter:site' content='@dunstewonline'>
+	<meta name='twitter:creator' content='@dunstewonline'>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 	<div class="hfeed site" id="page">
 		<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
-			<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+			<a class="skip-link sr-only sr-only-focusable" href="#content">Skip to content</a>
 			<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-transparent">
 				<div class="container" >
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url" class="navbar-brand d-lg-none" rel="home"><?php bloginfo( 'name' ); ?></a>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Duns Tew" itemprop="url" class="navbar-brand d-lg-none" rel="home">Duns Tew</a>
 					<button class="navbar-toggler p-0 border-0" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="navbar-toggler-icon"></span>
 					</button>
