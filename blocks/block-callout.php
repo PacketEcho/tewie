3bc29f23f3e8a3d2fbc7642de383acce3d9c1241
Packet Echo <packetecho@gmail.com>
2019-01-29 16:29:35 +0000

    removing old block definitions

--- blocks/block-callout.php
+++
@@ -1,6 +0,0 @@
-<div class="card border-primary mb-3" role="callout">
-  <div class="card-body">
-    <h4 class="card-title text-primary"><?php block_field( 'heading' ); ?></h4>
-    <p class="card-text"><?php block_field( 'content' ); ?></p>
-  </div>
-</div>
