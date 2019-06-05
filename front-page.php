a821925fc129d39693493b2b78f00aac0ff25bd1
Packet Echo <packetecho@gmail.com>
2019-06-05 18:52:15 +0100

    Fixing frontpage column layout on mobile

--- front-page.php
+++ front-page.php
@@ -3,14 +3,15 @@
 
 <div class="container" id="content" tabindex="-1">
 	<div class="row">
-		<div class="col-8">
+		<div class="col-12 col-md-8">
 			<?php do_action( 'tewwie_announcement_section', array('title' => "ANNOUNCEMENTS", 'posts_per_page' => 1) ); ?>
 			<?php do_action( 'tewwie_news_section', array('title' => "NEWS", 'posts_per_page' => 5, 'category' => null) ); ?>
 		</div><!-- .col -->
-		<div class="col">
+		<div class="col-12 col-md-4">
 			<aside class="pt-3 pb-5">
-				<h2 class="display-4">Events</h2>
+				<h2 class="display-4">EVENTS</h2>
 				<?php echo do_shortcode( '[calendar id="8940"]' ); ?>
+				<p><a href="http://packetecho.com/dt/calendar/all/" class="btn btn-lg btn-primary"><i class="far fa-calendar-alt"></i> More events</a></p>
 			</aside>
 		</div><!-- .col -->
 	</div><!-- .row -->
